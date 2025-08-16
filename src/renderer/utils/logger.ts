9fa73d57a0239579ca11ebd0767890ba0cb17fe0
FuTao <1980296464@qq.com>
2025-08-16 18:45:09 +0800

    更新日志级别 - 将Logger类的日志级别从DEBUG调整为INFO，以优化日志输出的可读性和重要性

--- src/renderer/utils/logger.ts
+++ src/renderer/utils/logger.ts
@@ -31,7 +31,7 @@ export class Logger {
 
   private constructor() {
     this.config = {
-      level: LogLevel.DEBUG,
+      level: LogLevel.INFO,
       enableConsole: true,
       enableStorage: true,
       maxStorageLogs: 1000,
